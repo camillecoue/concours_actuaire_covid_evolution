3026de3123314804c744682fefce8cc2ee96bf8c
MartinGdn <93950138+MartinGdn@users.noreply.github.com>
2021-12-08 18:37:15 +0100

    Add files via upload

--- graph_test.R
+++ graph_test.R
@@ -21,17 +21,11 @@ theme_set(theme_bw())
 data <- table_dep_finale8 %>% filter(Annee=="2021") %>% dplyr::select(-Annee)
 
 # Make a ggplot, but add frame=year: one image per year
-ggplot(data, aes(tx_hosp, taux_vaccin, size = densite, color = nomReg)) +
+ggplot(data, aes(taux_vaccin, tx_hosp, size = densite, color = nomReg)) +
   geom_point() +
   scale_x_log10() +
   theme_bw() +
   # gganimate specific bits:
-  labs(title = 'Semaine: {frame_time}', x = 'Taux hospi', y = 'Taux vaccin') +
+  labs(title = 'Semaine: {frame_time}', x = 'Taux vaccin', y = 'Taux hospi') +
   transition_time(semaine) +
   ease_aes('linear')
-
-# Save at gif:
-anim_save("271-ggplot2-animated-gif-chart-with-gganimate1.gif")
-
-animate(data, duration = 5, fps = 20, width = 200, height = 200, renderer = gifski_renderer())
-anim_save("output.gif")
